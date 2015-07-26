7ac611ea9a25868485e90b98acbb27c21775dfaf
hiroki <hiroki@notepc.local>
2015-07-27 02:24:26 +0900

    データディレクトリをボリュームを使用して上書きしても動作するように修正。 /etc/my.cnfのコピーをデータディレクトリに書き出すように修正

--- files/start_mysql.sh
+++ files/start_mysql.sh
@@ -1,6 +1,17 @@
-mysqld_safe --defaults-file=/etc/my.cnf &
+set -e
 
+MYSQL_DATA_DIR="/var/lib/mysql"
+MYSQL_DATABASE_DIR="${MYSQL_DATA_DIR}/mysql"
 
+if [ ! -d $MYSQL_DATABASE_DIR ]; then
+  mysql_install_db
+  cp /etc/my.cnf ${MYSQL_DATA_DIR}/my.cnf.template
+  chown -R mysql:mysql /var/lib/mysql
+fi
+
+mysqld_safe &
+
+set +e
 for i in `seq 1 20`;
 do
   mysqladmin ping
@@ -9,6 +20,7 @@ do
   fi
   sleep 1
 done
+set -e
 
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO root@'%' WITH GRANT OPTION"
 
